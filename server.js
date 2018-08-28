0a6c1e3bf77dd7b69afe3eaa39a1097ffbb0bcf8
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-08-28 11:53:16 -0700

    Added layout ejs, home ejs and a navbar partial. Looks terrible. need to fix navbar appearence and start building routes to other sections

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser'); //middleware to parse incoming bodies
 var express = require('express'); //web framework to allow for routing on webpage
 var ejsLayouts = require('express-ejs-layouts'); //Allows use of ejs format with express
 var flash = require('connect-flash'); //Allows for any error messages I may need
-var passport = require('./config/passportConfig'); //******* MAY NEED TO CHANGE THIS, DON'T FORGET ****** //A method of authentication
+//var passport = require('./config/passportConfig'); //******* MAY NEED TO CHANGE THIS, DON'T FORGET ****** //A method of authentication
 var session = require('express-session'); //Used to create sessions, 
 
 //Create express app
@@ -13,17 +13,23 @@ var app = express();
 app.set('view engine', 'ejs');
 
 //Set middlewares using app.use
-app.use(express.static(__dirname + '/public'));
-app.use(ejsLayouts);
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/public')); //allows use of static files like HTML or css
+app.use(ejsLayouts); //specifies use of ejs for front end views
+app.use(bodyParser.urlencoded({extended: false})); //Parses url encoded data
 
 //Layout initial routes
+app.get('/', function(req, res){
+	res.render('home')
+})
 
 
 
 
 
 //Listen
+app.listen(3000, function(){
+	console.log("You are now listening to the smooth sounds of Port 3000 in morning")
+})
 
 
 
