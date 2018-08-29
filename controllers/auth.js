688489eace329f480f94108aebe8abc32fd99d57
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-08-28 18:13:16 -0700

    added login form and updated auth controller to show the route is working. Now needs authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,17 +24,22 @@ router.post('/signup', function(req, res){
 	db.user.findOrCreate({ //Tells post route to create an account if it doesn't already exist
 		where: { email: req.body.email }, //This is what is searched to see if it exists, req.body.email = the email address enterd by user
 		defaults: req.body //inputs anything from req.body that needs to be filled in in my model
-	}).spread(function(user, newUser){ //First argument is the user info, second is a boolean telling whether a new entry was created
-		if(newUser){ //This if statement tells what to do if a new user is created
-			console.log('T or F - A new user was created ', newUser)
-			res.redirect('/profile') //Redirect to profile page if a new user was created
-		} else{
-			req.flash('Email already in use. Please login to see your profile.')
-			res.redirect('/auth/login') //Redirect to login page if account already exists
-		}
-	}).catch(function(err){
+		}).spread(function(user, newUser){ //First argument is the user info, second is a boolean telling whether a new entry was created
+			if(newUser){ //This if statement tells what to do if a new user is created
+				console.log('T or F - A new user was created ', newUser)
+				res.redirect('/profile') //Redirect to profile page if a new user was created
+			} else{
+				req.flash('Email already in use. Please login to see your profile.')
+				res.redirect('/auth/login') //Redirect to login page if account already exists
+			}
+		}).catch(function(err){
 		console.log('Error', err)
 	})
+});
+
+router.post('/login', function(req, res){
+	console.log(req.body);
+	res.send('You logged in')
 })
 
 
