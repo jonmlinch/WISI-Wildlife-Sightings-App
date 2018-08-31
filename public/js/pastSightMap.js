6ba08f77dfe2ef0ac1939dd4d2434b8a575920ce
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-08-31 13:50:45 -0700

    adding changes setting up file for deployment

--- public/js/pastSightMap.js
+++ public/js/pastSightMap.js
@@ -1,30 +1,30 @@
-var marker;
-var location;
+//var marker;
+// var location;
 
-function initMap(){
-  var myLatlng = new google.maps.LatLng(48.4779,-120.1862);
+// function initMap(){
+//   var myLatlng = new google.maps.LatLng(48.4779,-120.1862);
 
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatlng,
-    zoom: 10
-  });
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: myLatlng,
+//     zoom: 10
+//   });
 
-  function loadMarkers(){
-        console.log( 'HERE IS LOCATION DATA:', location)
-        for(var i = 0; i < location.length; i++){
-          var coordinate = location[i];
-          var position = new google.maps.LatLng(coordinate[0], coordinate[1]);
+//   function loadMarkers(){
+//         console.log( 'HERE IS LOCATION DATA:', location)
+//         for(var i = 0; i < location.length; i++){
+//           var coordinate = location[i];
+//           var position = new google.maps.LatLng(coordinate[0], coordinate[1]);
 
-          //Some code here to extend bounds if needed
+//           //Some code here to extend bounds if needed
 
-          var marker = new google.maps.Marker({
-            animation: google.maps.Animation.DROP,
-            map: map,
-            position: position
-          }) // end marker creation
+//           var marker = new google.maps.Marker({
+//             animation: google.maps.Animation.DROP,
+//             map: map,
+//             position: position
+//           }) // end marker creation
 
-        } //end for loop
-      } //end loadMarkers
+//         } //end for loop
+//       } //end loadMarkers
 
-    loadMarkers(); 
-}
+//     loadMarkers(); 
+// }
